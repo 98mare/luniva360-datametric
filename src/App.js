62ce68c7ba2178224f0eb53426e3b95c3f7817dd
root <root>
2022-06-09 17:27:20 +0545

    some component changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { Suspense, useEffect, useState } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import './App.css';
+import { AsyncDashBoard, AsyncPublicLayout } from './Components/AsyncComponent/asyncComponent';
 import SideBar from './Components/UI/SideBar';
 import Layouts from './Layouts/index'
+import PublicRoute from './Routes/PublicRoute';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 
 function App() {
   return (
-    <>
-      <Layouts></Layouts>
-    </>
+    <Layouts></Layouts>
   );
 }
 
---
+++ src/Pages/DatametricReport/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>data metric reports</div>
+  )
+}
+
+export default index
